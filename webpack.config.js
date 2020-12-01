199451595f58327bf18f81ce9639a4b985517217
i-am-ok-dont-worry <mateuspytel@gmail.com>
2020-12-01 11:12:24 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    mode: process.env.NODE_ENV !== 'production' ? 'development' : 'production',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: [/node_modules/, /dist/, /tests/],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            'config': path.resolve(__dirname, 'config')
+        }
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(process.cwd(), 'dist'),
+        library: 'LSFPlugin',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        umdNamedDefine: true
+    },
+    devtool: 'source-map',
+    optimization: {
+        minimize: true
+    }
+};
