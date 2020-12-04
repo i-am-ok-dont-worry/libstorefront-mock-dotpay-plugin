ff1b22358452982e9ca9b9369970bea4d5f9f9eb
Mateusz <mateusz.pytel@grupakmk.pl>
2020-12-05 00:17:17 +0100

    DotpayModuleState exported from module

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { MockDotpayPaymentPlugin } from './src/index';
 export { MockDotpayService } from './src/service/index';
 export { DotpayResponse, DotpayForm, DotpayStatus } from './src/types/index';
+export { DotpayModuleState } from './src/store/dotpay.default';
